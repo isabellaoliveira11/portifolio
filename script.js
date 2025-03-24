3100d2c95af67b53c3b78bc745ee34acf8499f31
isabellaoliveira11 <isabela676@gmail.com>
2025-03-24 11:40:15 -0300

    update - Atualizaçao na sessão contato

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import styled, { css } from "styled-components";
+
 // Filtro dos Projetos
 const filterButtons = document.querySelectorAll(".filter-button");
 const portfolioCards = document.querySelectorAll("#portfolio .card"); // Mudança aqui
@@ -22,3 +24,9 @@ filterButtons.forEach(button => {
     });
   });
 });
+
+function copiarEmail() {
+  const email = document.getElementById("email-para-copiar").textContent;
+  navigator.clipboard.writeText(email);
+  alert("Email copiado para a área de transferência!"); // Ou use uma notificação mais amigável
+}
