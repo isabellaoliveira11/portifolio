2c8c974a4f753042cc0ad73930680af14f362dbd
isabellaoliveira11 <isabela676@gmail.com>
2025-04-02 16:42:12 -0300

    update - Atualização na sessão portfólio

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
-import styled, { css } from "styled-components";
+function copyToClipboard() {
+  if (navigator.clipboard) {
+      const email = document.getElementById("emailToCopy").textContent;
+      navigator.clipboard.writeText(email)
+          .then(() => {
+              document.getElementById("copySuccess").style.display = "block";
+              setTimeout(() => {
+                  document.getElementById("copySuccess").style.display = "none";
+              }, 4000);
+          })
+          .catch(err => {
+              console.error('Falha ao copiar:', err);
+              alert('Falha ao copiar e-mail. Por favor, copie manualmente.');
+          });
+  } else {
+      alert('Seu navegador não suporta a cópia para a área de transferência.');
+  }
+}
 
-// Filtro dos Projetos
-const filterButtons = document.querySelectorAll(".filter-button");
-const portfolioCards = document.querySelectorAll("#portfolio .card"); // Mudança aqui
-
-filterButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    const category = button.dataset.category;
-
-    // Remove a classe "active" de todos os botões
-    filterButtons.forEach(btn => btn.classList.remove("active"));
-
-    // Adiciona a classe "active" ao botão clicado
-    button.classList.add("active");
-
-    // Filtra os cards
-    portfolioCards.forEach(card => { // Mudança aqui
-      if (card.dataset.category === category || category === "all") {
-        card.style.display = "block";
-      } else {
-        card.style.display = "none";
-      }
-    });
+function scrollToTop() {
+  window.scrollTo({
+      top: 0,
+      behavior: "smooth",
   });
-});
-
-function copiarEmail() {
-  const email = document.getElementById("email-para-copiar").textContent;
-  navigator.clipboard.writeText(email);
-  alert("Email copiado para a área de transferência!"); // Ou use uma notificação mais amigável
 }
